2957961d672a509c5d7502301b2c845b6a447eac
jhuang15 <jchuang1020@gmail.com>
2022-06-17 02:32:22 -0400

    Begin Header Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
 import Header from './components/header/Header';
-import Nav from './components/nav/Nav';
-import About from './components/about/About';
-import Experience from './components/experience/Experience';
-import Services from './components/services/Services';
-import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
+// import Nav from './components/nav/Nav';
+// import Experience from './components/experience/Experience';
+// import Services from './components/services/Services';
+// import Portfolio from './components/portfolio/Portfolio';
+// import Testimonials from './components/testimonials/Testimonials';
+// import Contact from './components/contact/Contact';
+// import Footer from './components/footer/Footer';
 
 //vvv Same as const App = () => { vvv
 function App() {
   return (
     <>
       <Header />
-      <Nav />
-      <About />
-      <Experience />
-      <Services />
-      <Portfolio />
-      <Testimonials />
-      <Contact />
-      <Footer />
     </>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import useState from 'react';
+import About from '../about/About';
+import Experience from '../experience/Experience';
+import Portfolio from '../portfolio/Portfolio';
+import Contact from '../contact/Contact';
 import './header.css';
 
 const Header = () => {
+  const [showPage, setShowPage] = useState(false);
+
+  const handleClick = event => {
+    setShowPage(true);
+  };
+
   return (
-    <div className="title">
-      <h1>Jenny Huang</h1>
+    <div className="container">
+      <div className="div-row">
+        <div className="table-of-content">
+          <div className="name">
+            <h1 id="first">Jenny</h1>
+            <h1 id="last">Huang</h1>
+          </div>
+          
+          <div className="content-btns">
+            <hr />
+            <button onClick={handleClick}>about me</button>
+            {showPage && (<About />)}
+            <button onClick={handleClick}>experience</button>
+            <Experience />
+            <button onClick={handleClick}>portfolio</button>
+            <Portfolio />
+            <button onClick={handleClick}>contact me</button>
+            <Contact />
+          </div>
+        </div>
+        <div className="div-column">
+          <div className="header-corner"> 
+            <h4>Jenny Huang</h4>
+            <p>SOFTWARE ENGINEER</p>
+            <p>FULL-STACK ENGINEER</p>
+          </div> 
+          <div className="head-photo">
+            <p>***PROFILE IMG HERE**</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
