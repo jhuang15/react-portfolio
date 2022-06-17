57760b9f5d35050eb0ecfb86e5949f6590a89ca5
jhuang15 <jchuang1020@gmail.com>
2022-06-17 16:14:26 -0400

    Started About me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,18 @@ import './about.css';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="container-wrapper">
+    <div className="left-container">
+      <p>img</p>
+    </div>
+    <div className="right-container">
+      <div className="aboutme-content">
+        <h2>About Me</h2>
+        <hr />
+        <p className="about-text">Inquisitive software engineer with a background in computer science transferring restaurant industry skills such as teamwork, attention to detail, and resiliency. Eager to utilize and strengthen qualifications independently and cooperatively to develop extensive products in technology to meet clients' needs.</p>
+      </div>
+    </div>
+    </div>
   )
 }
 
