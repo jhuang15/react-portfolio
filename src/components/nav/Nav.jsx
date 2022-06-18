2d946366b3d5766d152e9bc18cf2848b5b742905
jhuang15 <jchuang1020@gmail.com>
2022-06-18 02:07:24 -0400

    Starting nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './nav.css';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+      <h3>Menu</h3>
+      <Link className="btn-link" to="/about-me">about me</Link>           
+      <Link className="btn-link" to="/experience">experience</Link>         
+      <Link className="btn-link" to="/portfolio">portfolio</Link>
+      <Link className="btn-link" to="/contact-me">contact me</Link>
+    </div>
   )
 }
 
