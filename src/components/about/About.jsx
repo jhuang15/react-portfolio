f79a8c4e6949b56bb417942072038a2f213fbf24
jhuang15 <jchuang1020@gmail.com>
2022-10-30 18:33:56 -0400

    Commented out download resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
             <hr />
             <p className="about-text">Hi! My name is <b>Jenny Huang</b> and welcome to my portfolio! Here's a quick introduction about myself, I was born and raised in the silicon peach of <b>Atlanta, GA</b>. I graduated from <b>Georgia State University</b> with a bachelor's in computer science and recently completed an intense software engineering immersive course at <b>General Assembly</b>. I've always been interested in the field of technology and found I fit best in the middle of front-end and back-end development. My approach to life and code is simplicity at its finest: short and straight-forward code and minimalistic designs are my style.</p>
             <p className="about-text">Continue scrolling to view my projects ╰(*´︶`*)╯♡</p>
-            <a className="resume-btn" href="https://docs.google.com/document/d/1K5kgY5MvR8RJ68tS_MTj94XXROdn0gFAKBlOSF-4A60/export?format=pdf" download>Resume <HiOutlineDocumentDownload size={25}/></a>
+            {/* <a className="resume-btn" href="https://docs.google.com/document/d/1K5kgY5MvR8RJ68tS_MTj94XXROdn0gFAKBlOSF-4A60/export?format=pdf" download>Resume <HiOutlineDocumentDownload size={25}/></a> */}
           </div>
         </div>
       </div>
