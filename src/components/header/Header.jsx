45cb98c8da50313eb870860ed8ea1e881c28966a
jhuang15 <jchuang1020@gmail.com>
2023-03-02 16:27:50 -0500

    Small Changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -21,26 +21,9 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="container">
-      <div className="div-row">   
-        <div className="table-of-content">
-          <div className="name">
-            <h1 id="hi">Hi, I'm</h1>
-            <h1 id="last"><BiSearchAlt size={25} fill="#000000"/>Jenny Huang</h1>
-          </div>
+    
+    <img id="profile-pic" src="https://i.imgur.com/D0rFcIZ.jpg" alt="me"></img>
           
-        </div>
-        <div className="div-column">
-          <div className="header-corner"> 
-            <h4>Jenny Huang</h4>
-            <p>FULL-STACK DEVELOPER</p>
-          </div> 
-          <div className="head-photo">
-            <img id="profile-pic" src="https://i.imgur.com/D0rFcIZ.jpg" alt="me"></img>
-          </div>
-        </div>
-      </div>
-    </div>
   )
 }
 
