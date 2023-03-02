5d948a7178bfaf40cf8506b9a898979931554627
jhuang15 <jchuang1020@gmail.com>
2023-03-01 22:51:13 -0500

    Deleted Unused Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Header from './components/header/Header';
-//import { Routes, Route } from "react-router-dom";
 import About from './components/about/About';
 import Experience from './components/experience/Experience';
 import Portfolio from './components/portfolio/Portfolio';
@@ -10,14 +9,6 @@ import Footer from './components/footer/Footer';
 function App() {
   return (
     <>
-    {/* <Routes>
-      <Route exact path="/" element={<Header/>} />
-      <Route path="/about-me" element={<About/>} />
-      <Route path="/experience" element={<Experience/>} />
-      <Route path="/portfolio" element={<Portfolio/>} />
-      <Route path="/contact-me" element={<Contact/>} />
-    </Routes> */}
-
     <Header />
     <About />
     <Experience />
