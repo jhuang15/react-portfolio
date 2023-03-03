d15dbc78c6b5a92175059bd2cfb0a3a324b9620e
jhuang15 <jchuang1020@gmail.com>
2023-03-02 23:00:15 -0500

    Trying react-typical again

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './header.css';
-//import Typical from 'react-typical';
+import Typical from 'react-typical';
 
 const Header = () => {
   useEffect(function () {
@@ -23,19 +23,19 @@ const Header = () => {
     <div className="header-container">
       <div className="header-title-1-2">
         <div id="header-title-1">
-          {/* <Typical
+          <Typical
             steps={['Hey there!', 1000, 'I\'m Jenny Huang', 500]}
             wrapper="h1"
-          /> */}
-          <h1>Hi there! I'm Jenny Huang </h1>
+          />
+          {/* <h1>Hi there!<br /> I'm Jenny Huang </h1> */}
         </div>
         <div id="header-title-2">
-          {/* <Typical
+          <Typical
             steps={['Full-Stack Developer', 1000, 'Front-End Engineer', 1000, 'Software Engineer', 1000]}
             //loop={Infinity}
             //wrapper="h3"
-          /> */}
-          Software Engineer
+          />
+          {/* Software Engineer | Front-End Developer */}
         </div> 
       </div>
       
